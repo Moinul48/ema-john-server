4ac03c42a878b334ec3630773ba1c060224761c8
Moinul48 <mhmoinul@gmail.com>
2021-03-31 11:24:51 +0600

    finally deployed

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(bodyParser.json());
 app.use(cors());
 const port = 5000;
 
+app.get('/', (req, res) => {
+  res.send("Hello from db it's working")
+})
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
